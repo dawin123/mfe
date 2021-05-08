0342596807782e8b1c1f297ea3b11764a8354718
Dawin Widjaja <dawin.widjaja@foodpanda.com>
2021-05-08 18:36:10 +0800

    add AWS region, remove comment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,5 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    // add comment to trigger build
     return <div><h1>Hi There!</h1><hr/><MarketingApp/></div>
 };
