40647678c0f68fadefb09299bf1450227cc87c36
Dawin Widjaja <dawin.widjaja@foodpanda.com>
2021-05-08 18:26:36 +0800

    add comment to trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,6 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+    // add comment to trigger build
     return <div><h1>Hi There!</h1><hr/><MarketingApp/></div>
 };
